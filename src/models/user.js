bc5ccbebf63cee6195fabb22b3eac70e7adea01b
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-05 12:51:20 +0530

    add user schema

---
+++ src/models/user.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+export const User = mongoose.model("Users", UserSchema);
+
+// export const createTestingUser = async () => {
+//   try {
+//     const user = await User.create({
+//       name: "Vishal",
+//       email: "Vishalpathak",
+//       password: "visalpathak",
+//     });
+
+//     if (user) {
+//       console.log("User created Successfully : ", user);
+//       await TestingVideoFunc(user , "video uri")
+//       return;
+//     } 
+//     console.log("Error creating user");
+//   } catch (error) {
+//     throw error;
+//   }
+// };
