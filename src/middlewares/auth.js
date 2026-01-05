315a4f78ec0c2febba6a2712615f00b878b6b324
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-05 15:38:07 +0530

    add authentication middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,27 @@
+import { verifyAccessToken } from "../utils/jwt.js";
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const accessToken = req.headers?.authorization.split(" ")[1];
+
+    if (!accessToken) {
+      res.status(401).json({
+        message: "no access token , kindly login again",
+      });
+      return;
+    }
+
+    const payload = await verifyAccessToken(accessToken);
+    console.log(payload);
+    if (!payload) {
+      res.status(401).json({
+        message: "Access token expired , kindly login again",
+      });
+      return;
+    }
+    req.id = payload.userId;
+    next();
+  } catch (error) {
+    console.error("error in auth middleware :", error);
+  }
+};
