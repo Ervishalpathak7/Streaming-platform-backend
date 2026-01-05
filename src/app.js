f299a80e902e290ed736c0542fceb73c8a0ac53e
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-05 15:47:05 +0530

    add auth middlleware in the routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { configDotenv } from "dotenv";
 import userRouter from "./routes/user.js";
+import { authMiddleware } from "./middlewares/auth.js";
 
 
 configDotenv();
@@ -12,6 +13,10 @@ app.use(express.json({limit : "16kb"}));
 
 // routes
 app.use("/api" , userRouter)
+app.use(authMiddleware)
+app.use('/test' , (req , res) => {
+    res.send("okkkk")
+})
 
 
 export default app;
