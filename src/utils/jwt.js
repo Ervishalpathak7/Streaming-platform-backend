eb68e27cab7cf59d1d3133357189896f313ecd81
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-06 15:44:05 +0530

    update jwt token expiration time

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export const generateAccessToken = async (userId) => {
   try {
     return await jwt.sign({ userId: userId }, process.env.JWT_SECRET, {
-      algorithm: "HS256", expiresIn : "15m"
+      algorithm: "HS256", expiresIn : "30MINUTES"
     });
   } catch (error) {
     console.error("Error while generating access token :", error);
@@ -12,8 +12,9 @@ export const generateAccessToken = async (userId) => {
 
 export const verifyAccessToken = async (token) => {
   try {
-    return await jwt.verify(token, process.env.JWT_SECRET, { algorithms: "HS256" });
+    return jwt.verify(token, process.env.JWT_SECRET, { algorithms: "HS256" });
   } catch (error) {
+    if(error instanceof jwt.TokenExpiredError) return null;
     console.error("Error while verifying access token :", error);
   }
 };
