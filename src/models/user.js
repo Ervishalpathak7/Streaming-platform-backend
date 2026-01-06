30fd3f9854215618b007d2ebf2fae38c48b1055c
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-06 16:52:14 +0530

    remove testing function

--- src/models/user.js
+++ src/models/user.js
@@ -21,22 +21,3 @@ const UserSchema = new Schema({
 });
 
 export const User = mongoose.model("Users", UserSchema);
-
-// export const createTestingUser = async () => {
-//   try {
-//     const user = await User.create({
-//       name: "Vishal",
-//       email: "Vishalpathak",
-//       password: "visalpathak",
-//     });
-
-//     if (user) {
-//       console.log("User created Successfully : ", user);
-//       await TestingVideoFunc(user , "video uri")
-//       return;
-//     }
-//     console.log("Error creating user");
-//   } catch (error) {
-//     throw error;
-//   }
-// };
