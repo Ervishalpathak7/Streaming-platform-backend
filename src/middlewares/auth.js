3660396c652e777ee929a865f69bfeef4b36c207
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-06 15:46:52 +0530

    fix jwt error in login controller

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,16 +3,13 @@ import { verifyAccessToken } from "../utils/jwt.js";
 export const authMiddleware = async (req, res, next) => {
   try {
     const accessToken = req.headers?.authorization.split(" ")[1];
-
     if (!accessToken) {
       res.status(401).json({
         message: "no access token , kindly login again",
       });
       return;
     }
-
     const payload = await verifyAccessToken(accessToken);
-    console.log(payload);
     if (!payload) {
       res.status(401).json({
         message: "Access token expired , kindly login again",
@@ -22,6 +19,9 @@ export const authMiddleware = async (req, res, next) => {
     req.id = payload.userId;
     next();
   } catch (error) {
+    res.status(500).json({
+      message: "internal server error",
+    });
     console.error("error in auth middleware :", error);
   }
 };
