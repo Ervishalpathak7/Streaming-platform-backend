bb8e21a4248582734eaf97a76f05cd0dfc2e54c7
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-07 14:18:56 +0530

    update cloudinary upload function to add uploading , processing and db update asyn

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,10 +1,46 @@
+import { Video } from "../models/video.js";
 import { v2 } from "cloudinary";
-// Cloudinary config
+import fs from "fs";
 
-export const uploadToCloudinary = async (filepath) => {
+export const uploadVideoToCloudinary = async (fileId, filepath) => {
   try {
-    return await v2.uploader.upload(filepath);
+    const result = await v2.uploader.upload(filepath, {
+      resource_type: "video",
+      eager: [
+        {
+          streaming_profile: "auto",
+        },
+        {
+          format: "m3u8",
+          quality: "auto",
+        },
+        {
+          width: 320,
+          height: 240,
+          crop: "fill",
+          format: "jpg",
+          start_offset: "auto",
+        },
+      ],
+    });
+
+    await Video.findByIdAndUpdate(fileId, {
+      status: "READY",
+      url: result.eager?.[0]?.secure_url,
+      thumbnail: result.eager?.[1]?.secure_url ?? null,
+    });
   } catch (error) {
-    console.error("Error while uploading to cloudinary :", error);
+    await Video.findByIdAndUpdate(fileId, {
+      status: "FAILED",
+      url: null,
+      thumbnail: null,
+    });
+    console.error("Error in video processing :", error);
+  } finally {
+    if (filepath) {
+      fs.unlink(filepath, (err) => {
+        if (err) console.error("Error deleting file : ", err);
+      });
+    }
   }
 };
