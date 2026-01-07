63690d25e3febed0ee8a4604f55d84d62eff7e87
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-07 14:19:08 +0530

    update video uploading controller

--- src/controller/video.js
+++ src/controller/video.js
@@ -1,54 +1,27 @@
-import { uploadToCloudinary } from "../utils/cloudinary.js";
 import { Video } from "../models/video.js";
-import fs from "fs";
-let filepath;
+import { uploadVideoToCloudinary } from "../utils/cloudinary.js";
 
 export const uploadVideoController = async (req, res) => {
   try {
     const uploadedFile = req?.file;
     if (!uploadedFile) {
-      res.status(500).json({
-        message: "Error Uploading file",
-      });
+      res.status(400).json({ message: "No file uploaded" });
       return;
     }
-    filepath = uploadedFile.path;
-    // upload it to cloudinary
-    const uploadedFileToCloudinary = await uploadToCloudinary(filepath);
-    // save data into mongodb
     const savedVideo = await Video.create({
-      owner: req.userId,
-      url: uploadedFileToCloudinary.secure_url,
-      filename: uploadedFileToCloudinary.original_filename,
+      owner: req.id,
+      status: "PROCESSING",
+      filename: uploadedFile.originalname,
     });
-
-    if (savedVideo) {
-      res.status(200).json({
-        message: "Video uploaded succesfully",
-        video: {
-          id: savedVideo._id,
-          name: savedVideo.filename,
-          url: savedVideo.url,
-        },
-      });
-      return;
-    }
-
-    res.status(500).json({
-      message: "Error Saving video data",
-      url: "fileUrl",
+    res.status(200).json({
+      message: "Video processing",
+      videoId: savedVideo._id,
     });
-    return;
+    uploadVideoToCloudinary(savedVideo._id, uploadedFile.path);
   } catch (error) {
     res.status(500).json({
       message: "internal server error",
     });
     console.error("error in video uploading controller :", error);
-  } finally {
-    if (filepath) {
-      fs.unlink(filepath, (err) => {
-        if (err) throw err;
-      });
-    }
   }
 };
