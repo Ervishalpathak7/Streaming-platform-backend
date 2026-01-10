3e6dbe62769316425d961dacff34af66b094eb80
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-10 15:34:01 +0530

    add file clearning in the video uploading controller

--- src/controller/video.js
+++ src/controller/video.js
@@ -3,37 +3,48 @@ import { AppError } from "../error/index.js";
 import { Video } from "../models/video.js";
 import { uploadVideoToCloudinary } from "../utils/cloudinary.js";
 import { logger } from "../utils/winston.js";
+import { fileClearing } from "../utils/fileClearning.js";
 
-export const uploadVideoController = async (req, res) => {
-  const idempotencyKey = req?.headers?.idempotencykey;
-  if (!idempotencyKey) throw new AppError("No Idempotency Key", 400);
+export const createVideoController = async (req, res) => {
   const uploadedFile = req.file;
   if (!uploadedFile) throw new AppError("Invalid file", 400);
+  try {
+    const idempotencyKey = req?.headers?.idempotencykey;
+    if (!idempotencyKey) throw new AppError("No Idempotency Key", 400);
+    const { title, description } = req.body;
+    if (!title) throw new AppError("Title is required", 400);
 
-  const ExistingVideo = await Video.findOne({ idempotencyKey: idempotencyKey });
-  if (ExistingVideo) {
-    res.status(200).json({
-      message: ExistingVideo.status,
-      videoId: ExistingVideo._id,
+    const ExistingVideo = await Video.findOne({
+      idempotencyKey: idempotencyKey,
     });
-    return;
+    if (ExistingVideo) {
+      res.status(200).json({
+        message: ExistingVideo.status,
+        videoId: ExistingVideo._id,
+      });
+      return;
+    }
+
+    const savedVideo = await Video.create({
+      owner: req.userId,
+      status: "PROCESSING",
+      title,
+      description,
+      filename: uploadedFile.originalname,
+      idempotencyKey: idempotencyKey,
+    });
+
+    res.status(200).json({
+      message: "Video processing",
+      videoId: savedVideo._id,
+    });
+
+    logger.info(`A New Video Recieved : ${savedVideo._id}`);
+    uploadVideoToCloudinary(savedVideo._id, uploadedFile.path);
+  } catch (error) {
+    fileClearing(uploadedFile.path);
+    throw error;
   }
-
-  const savedVideo = await Video.create({
-    owner: req.userId,
-    status: "PROCESSING",
-    filename: uploadedFile.originalname,
-    idempotencyKey: idempotencyKey,
-  });
-
-  res.status(200).json({
-    message: "Video processing",
-    videoId: savedVideo._id,
-  });
-
-  logger.info(`A New Video Recieved : ${savedVideo._id}`);
-
-  uploadVideoToCloudinary(savedVideo._id, uploadedFile.path);
 };
 
 export const getVideoController = async (req, res) => {
