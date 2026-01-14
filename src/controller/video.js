209680f285a18555530835323a63852fc4b9548c
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-14 13:09:58 +0530

    add caching function in controller

--- src/controller/video.js
+++ src/controller/video.js
@@ -4,6 +4,7 @@ import { Video } from "../models/video.js";
 import { uploadVideoToCloudinary } from "../utils/cloudinary.js";
 import { logger } from "../utils/winston.js";
 import { fileClearing } from "../utils/fileClearning.js";
+import { saveVideoData, getVideoData } from "../cache/index.js";
 
 export const createVideoController = async (req, res) => {
   const uploadedFile = req.file;
@@ -38,9 +39,9 @@ export const createVideoController = async (req, res) => {
       message: "Video processing",
       videoId: savedVideo._id,
     });
-
+    saveVideoData(savedVideo._id, savedVideo.status, title);
     logger.info(`A New Video Recieved : ${savedVideo._id}`);
-    uploadVideoToCloudinary(savedVideo._id, uploadedFile.path);
+    uploadVideoToCloudinary(savedVideo._id, uploadedFile.path, title);
   } catch (error) {
     fileClearing(uploadedFile.path);
     throw error;
@@ -54,12 +55,28 @@ export const getVideoController = async (req, res) => {
   if (!mongoose.isValidObjectId(videoId))
     throw new AppError("Invalid video Id", 404);
 
+  const cachedVideo = await getVideoData(videoId);
+  if (cachedVideo) {
+    res.status(200).json({
+      videoId,
+      title: cachedVideo.title,
+      status: cachedVideo.status,
+      streamUrl: cachedVideo.url || null,
+      thumbnail: cachedVideo.thumbnail || null,
+    });
+    logger.info("Cache Hit");
+    return;
+  }
+  logger.info("Cache Miss");
+
   const video = await Video.findById(videoId);
   if (!video) throw new AppError("No video found", 404);
   res.status(200).json({
     videoId: video._id,
+    title: video.title,
     status: video.status,
     streamUrl: video.status === "READY" ? video.url : null,
     thumbnail: video.thumbnail ?? null,
   });
+  saveVideoData(videoId, video.status, video.title, video.url, video.thumbnail);
 };
