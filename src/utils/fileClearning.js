e510a5959cab3c9f078851526d1ac521a2bac093
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-14 14:33:30 +0530

    fix file cleaning function

--- src/utils/fileClearning.js
+++ src/utils/fileClearning.js
@@ -1,8 +1,16 @@
 import fs from "fs";
 import { logger } from "./winston.js";
+import fs from "fs/promises";
 
 export const fileClearing = async (filepath) => {
-  fs.unlink(filepath, (err) => {
-    if (err) logger.error("Error while clearning file");
-  });
+  if (!filepath) return;
+
+  try {
+    await fs.unlink(filepath);
+  } catch (err) {
+    logger.error(
+      "Temp file cleanup failed",
+      { filepath, message: err.message, stack: err.stack }
+    );
+  }
 };
