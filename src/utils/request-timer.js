feb1edeb060fac9bfc895702ffc4e1c656eee8fa
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-19 13:42:57 +0530

    code refining

--- src/utils/request-timer.js
+++ src/utils/request-timer.js
@@ -1,15 +1,9 @@
 import { logger } from "./winston.js";
 export const requestTimer = (req, res, next) => {
   const start = process.hrtime.bigint();
-
   res.on("finish", () => {
     const end = process.hrtime.bigint();
     const durationMs = Number(end - start) / 1e6;
-
-    logger.info(
-      `${req.method} ${req.originalUrl} → ${res.statusCode} | ${durationMs.toFixed(2)} ms`
-    );
-  });
-
+    logger.info( `${req.method} ${req.originalUrl} → ${res.statusCode} | ${durationMs.toFixed(2)} ms`) });
   next();
 };
