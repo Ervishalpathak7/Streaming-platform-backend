797a719bbc928c4b27ce1bf2e1227fea51b45414
Vishal Pathak <vishalpathak.er@gmail.com>
2026-01-29 15:42:33 +0530

    add cors to allow frontend req

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ import authRouter from "./routes/user.js";
 import { errorHandler } from "./middlewares/errorHandeling.js";
 import videoRouter from "./routes/video.js";
 import { requestTimer } from "./utils/request-timer.js";
+import cors from "cors"
 
 const app = express();
 app.set("trust proxy", true);
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.json({ limit: "16kb" }));
