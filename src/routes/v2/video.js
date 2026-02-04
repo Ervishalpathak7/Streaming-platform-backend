69bba1c664bf5cd4df4994ec57e0cc5f8ba0952a
Vishal Pathak <vishalpathak.er@gmail.com>
2026-02-05 01:01:24 +0530

    add complete upload route

--- src/routes/v2/video.js
+++ src/routes/v2/video.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getSignedUrlControllerV2, initUploadControllerV2 } from "../../controller/v2/videoController.js";
+import { completeUploadControllerV2, getSignedUrlControllerV2, initUploadControllerV2 } from "../../controller/v2/videoController.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 import { authMiddleware } from "../../middlewares/auth.js";
 import { rateLimitMiddleware } from "../../middlewares/rateLimiting.js";
@@ -18,5 +18,11 @@ v2VideoRouter.get("/signed-urls/:videoId",
     rateLimitMiddleware("UPLOAD", req => req.userId || req.ip),
     asyncHandler( getSignedUrlControllerV2 )
 )
+
+v2VideoRouter.post("/complete/:videoId",
+    authMiddleware,
+    rateLimitMiddleware("UPLOAD", req => req.userId || req.ip),
+    asyncHandler(completeUploadControllerV2)
+);
 
 export default v2VideoRouter;
