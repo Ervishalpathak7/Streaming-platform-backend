da8107db3fad0ffac1a15f8d8308e4fc267d90d0
Vishal Pathak <vishalpathak.er@gmail.com>
2026-02-04 15:27:20 +0530

    add v2 controller for video uploads

---
+++ src/controller/v2/videoController.js
@@ -0,0 +1,121 @@
+import { v4 as uuidv4 } from "uuid";
+import path from "path";
+import { AppError } from "../../error/index.js";
+import { logger } from "../../utils/winston.js";
+import { createMultipartUpload } from "../../services/s3.js";
+import { Video } from "../../models/video.js";
+import { getUploadPartUrls } from "../../services/s3.js";
+import { CHUNK_SIZE } from "../../config.js";
+import mongoose from "mongoose";
+
+export const initUploadControllerV2 = async (req, res) => {
+    const { title, description, filename, filesize, mimetype } = req.body || {};
+    const idempotencyKey = req.headers["idempotency-key"] || null;
+
+    if (!idempotencyKey)
+        throw new AppError("Idempotency key is required", 400);
+
+    if (!title || title.trim().length < 3)
+        throw new AppError("Title is required", 400);
+
+    if (!filename || filename.trim().length === 0)
+        throw new AppError("Invalid filename", 400);
+
+    if (!filesize || filesize <= 0)
+        throw new AppError("Invalid file size", 400);
+
+    if (!mimetype || !mimetype.startsWith("video/"))
+        throw new AppError("Invalid MIME type", 400);
+
+    if (filesize > 1 * 1024 * 1024 * 1024)
+        throw new AppError("Max file size is 1GB", 400);
+
+    const ext = path.extname(filename).toLowerCase() || ".mp4";
+    if (![".mp4", ".mkv", ".mov", ".avi"].includes(ext))
+        throw new AppError("Unsupported file extension", 400);
+
+    try {
+        const totalParts = Math.ceil(filesize / CHUNK_SIZE);
+        const existingVideo = await Video.findOne({ owner: req.userId, idempotencyKey, filename, size: filesize });
+        if (existingVideo) {
+            if (existingVideo.status === "UPLOADED" || existingVideo.status === "PROCESSING") {
+                return res.status(200).json({
+                    data: {
+                        videoId: existingVideo._id,
+                        status: existingVideo.status,
+                    },
+                });
+            }
+            if (existingVideo.status === "INITIATED") {
+                return res.status(200).json({
+                    data: {
+                        videoId: existingVideo._id,
+                    },
+                });
+            }
+        }
+
+        const objectId = uuidv4();
+        const key = `v2/videos/${objectId}${ext}`;
+        const uploadData = await createMultipartUpload(key, mimetype);
+        console.log("Upload Data:", uploadData);
+        const video = await Video.create({
+            owner: req.userId,
+            title: title.trim(),
+            description: description || "",
+            filename: filename,
+            s3UploadId: uploadData.uploadId,
+            totalChunks: totalParts,
+            status: "INITIATED",
+            s3Key: key,
+            size: filesize,
+            idempotencyKey: idempotencyKey,
+        });
+        res.status(201).json({
+            data: {
+                videoId: video._id,
+                status: video.status,
+            },
+        });
+
+    } catch (error) {
+        logger.error("Error in initUploadControllerV2:", error);
+        throw new AppError("Could not initiate upload", 500);
+    }
+};
+
+export const getSignedUrlControllerV2 = async (req, res) => {
+    const { videoId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(videoId))
+        throw new AppError("Invalid video ID", 400);
+    
+    if (!videoId)
+        throw new AppError("Video ID is required", 400);
+
+    try {
+        const video = await Video.findOne({ _id: videoId, owner: req.userId });
+
+        if (video.status === "FAILED")
+            throw new AppError("Upload previously failed. Please re-initiate upload.", 400);
+        if (video.status !== "INITIATED") {
+            return res.status(200).json({
+                data: {
+                    videoId: video._id,
+                    status: video.status,
+                    message: `Video is currently ${video.status}`,
+                },
+            });
+        }
+        const signedUrls = await getUploadPartUrls(video.s3Key, video.s3UploadId, video.totalChunks);
+        res.status(200).json({
+            data: {
+                videoId: video._id,
+                signedUrls,
+            },
+        });
+    } catch (error) {
+        logger.error("Error in getSignedUrlControllerV2:", error);
+        throw new AppError("Could not get signed URL", 500);
+    }
+}
