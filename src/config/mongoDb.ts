b98a6960e67fdb164ad1dab32444fa0541e98133
Vishal Pathak <vishalpathak.er@gmail.com>
2026-02-09 12:16:16 +0530

    update mongodb connection and config file

--- src/config/mongoDb.ts
+++ src/config/mongoDb.ts
@@ -1,3 +1,4 @@
+import { AppError } from "@/error/index.js";
 import logger from "@/lib/winston.js";
 import mongoose from "mongoose";
 
@@ -11,25 +12,30 @@ if (!process.env.MONGO_URI) {
   throw new Error("MONGO_URI environment variable is not set");
 }
 
-export const connectDb = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI as string, {
-      maxPoolSize: 10,
-      serverSelectionTimeoutMS: 5000,
-      dbName: "streamKaro",
-    });
-    logger.info("Database connection established");
-  } catch (error) {
-    logger.error("Database connection failed", {
-      category: "server",
-      service: "db",
-      lifecycle: "startup",
-      code: "DB_CONNECTION_FAILED",
-      error,
-    });
-    throw error;
-  }
-};
+try {
+  await mongoose.connect(process.env.MONGO_URI, {
+    maxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    dbName: "streamKaro",
+  });
+  logger.info("Database connection established");
+} catch (error) {
+  logger.error("Database connection failed", {
+    category: "server",
+    service: "db",
+    lifecycle: "startup",
+    code: "DB_CONNECTION_FAILED",
+    error,
+  });
+
+  if (error instanceof Error)
+    throw new AppError("Database connection failed", 500, error);
+  throw new AppError(
+    "Database connection failed",
+    500,
+    new Error("Unknown database connection error"),
+  );
+}
 
 export const disconnectDb = async () => {
   try {
