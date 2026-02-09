b8fd39d97382f4298e9d55a7beb3c641cd892fa2
Vishal Pathak <vishalpathak.er@gmail.com>
2026-02-09 16:03:29 +0530

    update error class

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,71 +1,56 @@
 export class AppError extends Error {
   originalError: Error | null;
-  statusCode: number;
   errorMessage: string;
-  isOperational: boolean;
   constructor(
     message: string,
-    statusCode: number,
+    name: string,
     originalError: Error | null = null,
   ) {
     super(message);
-    this.statusCode = statusCode;
+    this.name = name;
     this.errorMessage = message;
-    this.isOperational = true;
     this.originalError = originalError;
   }
 }
 
 export class ValidationError extends AppError {
-  constructor(message: string, originalError: Error | null = null) {
-    super(message, 400, originalError);
+  constructor(message: string) {
+    super(message, "ValidationError");
   }
 }
 
 export class NotFoundError extends AppError {
-  constructor(message: string, originalError: Error | null = null) {
-    super(message, 404, originalError);
+  constructor(message: string) {
+    super(message, "NotFoundError");
   }
 }
 
 export class InternalServerError extends AppError {
-  constructor(message: string, originalError: Error | null = null) {
-    super(message, 500, originalError);
+  constructor(message: string , originalError: Error | null = null) {
+    super(message, "InternalServerError", originalError);
   }
 }
 
 export class UnauthorizedError extends AppError {
-  constructor(message: string, originalError: Error | null = null) {
-    super(message, 401, originalError);
+  constructor(message: string) {
+    super(message, "UnauthorizedError");
   }
 }
 
 export class ForbiddenError extends AppError {
-  constructor(message: string, originalError: Error | null = null) {
-    super(message, 403, originalError);
+  constructor(message: string) {
+    super(message, "ForbiddenError");
   }
 }
 
 export class ConflictError extends AppError {
-  constructor(message: string, originalError: Error | null = null) {
-    super(message, 409, originalError);
-  }
-}
-
-export class BadRequestError extends AppError {
-  constructor(message: string, originalError: Error | null = null) {
-    super(message, 400, originalError);
-  }
-}
-
-export class ServiceUnavailableError extends AppError {
-  constructor(message: string, originalError: Error | null = null) {
-    super(message, 503, originalError);
+  constructor(message: string) {
+    super(message, "ConflictError");
   }
 }
 
 export class TooManyRequestsError extends AppError {
-  constructor(message: string, originalError: Error | null = null) {
-    super(message, 429, originalError);
+  constructor(message: string) {
+    super(message, "TooManyRequestsError");
   }
 }
