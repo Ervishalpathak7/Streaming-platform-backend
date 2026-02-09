94c59722ac95e0c832a2c5be445ba186cdbfb0de
Vishal Pathak <vishalpathak.er@gmail.com>
2026-02-09 11:42:04 +0530

    last commit was incomplete

--- src/error/index.js
+++
@@ -1,9 +0,0 @@
-export class AppError extends Error {
-  constructor(message, statusCode, originalError = null) {
-    super(message);
-    this.statusCode = statusCode;
-    this.errorMessage = message;
-    this.isOperational = true;
-    this.originalError = originalError;
-  }
-}
--- src/error/index.ts
+++ src/error/index.ts
@@ -15,3 +15,57 @@ export class AppError extends Error {
     this.originalError = originalError;
   }
 }
+
+export class ValidationError extends AppError {
+  constructor(message: string, originalError: Error | null = null) {
+    super(message, 400, originalError);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string, originalError: Error | null = null) {
+    super(message, 404, originalError);
+  }
+}
+
+export class InternalServerError extends AppError {
+  constructor(message: string, originalError: Error | null = null) {
+    super(message, 500, originalError);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string, originalError: Error | null = null) {
+    super(message, 401, originalError);
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message: string, originalError: Error | null = null) {
+    super(message, 403, originalError);
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(message: string, originalError: Error | null = null) {
+    super(message, 409, originalError);
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor(message: string, originalError: Error | null = null) {
+    super(message, 400, originalError);
+  }
+}
+
+export class ServiceUnavailableError extends AppError {
+  constructor(message: string, originalError: Error | null = null) {
+    super(message, 503, originalError);
+  }
+}
+
+export class TooManyRequestsError extends AppError {
+  constructor(message: string, originalError: Error | null = null) {
+    super(message, 429, originalError);
+  }
+}
