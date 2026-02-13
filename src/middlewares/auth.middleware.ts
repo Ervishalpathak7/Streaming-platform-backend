28fe45b53709bf3a2e584831db8ee77ad2b867e9
Vishal Pathak <vishalpathak.er@gmail.com>
2026-02-13 10:48:47 +0530

    add auth middleware for rest apis

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,5 +1,7 @@
 import { TRPCError } from "@trpc/server";
 import { middleware } from "@/trpc/trpc.js";
+import type { NextFunction, Request, Response } from "express";
+import { verifyAccessToken } from "@/lib/jwt";
 
 export const isAuthed = middleware(({ ctx, next }) => {
   if (!ctx.userId) {
@@ -14,3 +16,26 @@ export const isAuthed = middleware(({ ctx, next }) => {
     },
   });
 });
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  const token = authHeader.split(" ")[1] || "";
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  try {
+    const decoded = verifyAccessToken(token);
+    req.userId = decoded.userId;
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+};
