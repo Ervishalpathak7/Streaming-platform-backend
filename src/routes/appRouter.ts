2d82a98a673bf5c9a9880ad923f317a19de1c567
Vishal Pathak <vishalpathak.er@gmail.com>
2026-02-13 12:45:41 +0530

    register v1 auth routes

--- src/routes/appRouter.ts
+++ src/routes/appRouter.ts
@@ -1,4 +1,12 @@
+import { Router } from "express";
 import { v3Router } from "./v3/v3router.js";
+import v1AuthRouter from "./v1/auth.js";
+
+
+export const appRouter = Router();
+
+appRouter.use("/auth", v1AuthRouter);
+
 
 export const trpcRouter = {
   v3 : v3Router
