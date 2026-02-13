b19fbfa66be5dedc60082b2a5d5f283fcbc834ea
Vishal Pathak <vishalpathak.er@gmail.com>
2026-02-13 10:59:41 +0530

    add async handler for rest based apis

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,11 @@
+import type { Request, Response, NextFunction } from "express";
+
+export const asyncHandler = (fn: Function) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
