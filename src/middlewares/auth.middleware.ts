39b4d69ca7cb66724c3a2c6df5a6ce722a03ef73
Vishal Pathak <vishalpathak.er@gmail.com>
2026-02-13 11:45:57 +0530

    update auth middleware with blacklist validation

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,6 +2,9 @@ import { TRPCError } from "@trpc/server";
 import { middleware } from "@/trpc/trpc.js";
 import type { NextFunction, Request, Response } from "express";
 import { verifyAccessToken } from "@/lib/jwt";
+import { isTokenBlacklisted } from "@/services/auth.service";
+import { InternalServerError, UnauthorizedError } from "@/error";
+import logger from "@/lib/winston";
 
 export const isAuthed = middleware(({ ctx, next }) => {
   if (!ctx.userId) {
@@ -17,25 +20,41 @@ export const isAuthed = middleware(({ ctx, next }) => {
   });
 });
 
-export const authMiddleware = (
+export const authMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
+  if (!authHeader || !authHeader.startsWith("Bearer "))
+    throw new UnauthorizedError("Authorization header missing or malformed");
 
   const token = authHeader.split(" ")[1] || "";
-  if (!token) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
+  if (!token)
+    throw new UnauthorizedError("Token missing from Authorization header");
+
   try {
+    const isBlacklisted = await isTokenBlacklisted(token);
+    if (isBlacklisted) throw new UnauthorizedError("Token is blacklisted");
     const decoded = verifyAccessToken(token);
     req.userId = decoded.userId;
+
     return next();
   } catch (err) {
-    return res.status(401).json({ error: "Unauthorized" });
+    if (err instanceof UnauthorizedError || err instanceof InternalServerError)
+      throw err;
+    logger.error("Error in auth middleware", {
+      token: token,
+      req: {
+        method: req.method,
+        url: req.url,
+        headers: req.headers,
+      },
+      error: err instanceof Error ? err.stack : String(err),
+    });
+    throw new InternalServerError(
+      "An error occurred while processing the token",
+      err instanceof Error ? err : new Error(String(err)),
+    );
   }
 };
